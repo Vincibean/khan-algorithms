01e7e20fd86a050cd8fecf2a0937bce280f18498
Vincibean <andrebessi00@gmail.com>
2019-06-08 19:40:11 +0100

    using right order for currying

--- haskell/src/Algo/BinarySearch.hs
+++ haskell/src/Algo/BinarySearch.hs
@@ -3,15 +3,15 @@ module Algo.BinarySearch
   )
 where
 
-binarySearch :: (Num a, Integral a) => [a] -> a -> Maybe Int
-binarySearch haystack needle =
-  binarySearchRec haystack needle 0 (length haystack)
+binarySearch :: (Num a, Integral a) => a -> [a] -> Maybe Int
+binarySearch needle haystack =
+  binarySearchRec needle haystack 0 (length haystack)
 
-binarySearchRec :: (Num a, Integral a) => [a] -> a -> Int -> Int -> Maybe Int
-binarySearchRec haystack needle lo hi
+binarySearchRec :: (Num a, Integral a) => a -> [a] -> Int -> Int -> Maybe Int
+binarySearchRec needle haystack lo hi
   | hi < lo        = Nothing
-  | pivot > needle = binarySearchRec haystack needle lo (mid - 1)
-  | pivot < needle = binarySearchRec haystack needle (mid + 1) hi
+  | pivot > needle = binarySearchRec needle haystack lo (mid - 1)
+  | pivot < needle = binarySearchRec needle haystack (mid + 1) hi
   | otherwise      = Just mid
  where
   mid   = lo + (hi - lo) `div` 2
--- haskell/test/Algo/BinarySearchSpec.hs
+++ haskell/test/Algo/BinarySearchSpec.hs
@@ -42,14 +42,14 @@ spec = do
         it
                 "returns the index of the value if present (first position, 1 elements array)"
             $ do
-                  binarySearch [1] 1 `shouldBe` Just 0
+                  binarySearch 1 [1] `shouldBe` Just 0
         it
                 "returns the index of the value if present (second position, 2 elements array)"
             $ do
-                  binarySearch [0, 1] 1 `shouldBe` Just 1
+                  binarySearch 1 [0, 1] `shouldBe` Just 1
         it "returns the index of the value if present (middle position)" $ do
-            binarySearch primes 73 `shouldBe` Just 20
+            binarySearch 73 primes `shouldBe` Just 20
         it "returns Nothing if it couldn't find a value in an array" $ do
-            binarySearch primes 72 `shouldBe` Nothing
+            binarySearch 72 primes `shouldBe` Nothing
         it "returns the index of the value if present (last position)" $ do
-            binarySearch primes 97 `shouldBe` Just 24
+            binarySearch 97 primes `shouldBe` Just 24
