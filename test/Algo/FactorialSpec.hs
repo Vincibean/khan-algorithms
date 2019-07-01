fa7f4eecf96ad91c20e75d416450f62eb477636e
Vincibean <andrebessi00@gmail.com>
2019-07-02 00:26:41 +0100

    clean code

--- test/Algo/FactorialSpec.hs
+++ test/Algo/FactorialSpec.hs
@@ -4,14 +4,13 @@ module Algo.FactorialSpec
 where
 
 import           Test.Hspec
-import           Algo.Factorial            ( iterFactorial )
+import           Algo.Factorial                 ( iterFactorial )
 
 
 spec :: Spec
 spec = describe "iterFactorial" $ do
-    it "returns 1 if 0 is given" $          
-        iterFactorial 0 `shouldBe` 1
-    it "returns 1 if 1 is given" $          
-        iterFactorial 1 `shouldBe` 1
-    it "returns a factorial if a positive integer is given" $
-        iterFactorial 5 `shouldBe` (5 * 4 * 3 * 2 * 1)
+    it "returns 1 if 0 is given" $ iterFactorial 0 `shouldBe` 1
+    it "returns 1 if 1 is given" $ iterFactorial 1 `shouldBe` 1
+    it "returns a factorial if a positive integer is given"
+        $          iterFactorial 5
+        `shouldBe` (5 * 4 * 3 * 2 * 1)
