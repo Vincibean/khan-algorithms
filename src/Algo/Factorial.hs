dda1e00a1187c075726cd06a1418a99a01904663
Vincibean <andrebessi00@gmail.com>
2019-07-02 20:05:34 +0100

    added recursive factorial

--- src/Algo/Factorial.hs
+++ src/Algo/Factorial.hs
@@ -1,7 +1,12 @@
 module Algo.Factorial
     ( iterFactorial
+    , recFactorial
     )
 where
 
-iterFactorial :: Enum a => Num a => a -> a
+iterFactorial :: (Enum a, Num a) => a -> a
 iterFactorial n = product [1 .. n]
+
+recFactorial :: (Eq a, Num a) => a -> a
+recFactorial 0 = 1
+recFactorial n = n * recFactorial (n - 1)
--- test/Algo/FactorialSpec.hs
+++ test/Algo/FactorialSpec.hs
@@ -4,13 +4,29 @@ module Algo.FactorialSpec
 where
 
 import           Test.Hspec
-import           Algo.Factorial                 ( iterFactorial )
+import           Algo.Factorial                 ( iterFactorial
+                                                , recFactorial
+                                                )
 
 
 spec :: Spec
-spec = describe "iterFactorial" $ do
+spec = do
+    iterFactorialSpec
+    recFactorialSpec
+
+iterFactorialSpec :: Spec
+iterFactorialSpec = describe "iterFactorial" $ do
     it "returns 1 if 0 is given" $ iterFactorial 0 `shouldBe` 1
     it "returns 1 if 1 is given" $ iterFactorial 1 `shouldBe` 1
     it "returns a factorial if a positive integer is given"
         $          iterFactorial 5
+        `shouldBe` (5 * 4 * 3 * 2 * 1)
+
+
+recFactorialSpec :: Spec
+recFactorialSpec = describe "recFactorial" $ do
+    it "returns 1 if 0 is given" $ recFactorial 0 `shouldBe` 1
+    it "returns 1 if 1 is given" $ recFactorial 1 `shouldBe` 1
+    it "returns a factorial if a positive integer is given"
+        $          recFactorial 5
         `shouldBe` (5 * 4 * 3 * 2 * 1)
